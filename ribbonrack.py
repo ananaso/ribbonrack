93f8b463fe99249e81b632eee8181405b83b1a72
Alden Davidson <adavidson@protonmail.ch>
2019-05-20 16:09:08 -0400

    initial window boilerplate

---
+++ ribbonrack.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''
+Helps build a ribbon rack in the proper order.
+
+Author: Alden Davidson, adavidson@protonmail.ch
+Date: Summer 2019
+'''
+
+# en.wikipedia.org/wiki/PyQt#PyQt5
+
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QStackedWidget,
+)
+from PyQt5.QtCore import (
+        Qt
+)
+
+class MainWindow(QMainWindow):
+    '''
+    MainWindow controls display of application.
+    Different "screens" are achieved by switching QWidgets
+    '''
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        # window options
+        self.setWindowTitle("RibbonRack")
+        self.resize(1280, 700)
+
+    def keyPressEvent(self, event): # pylint: disable=invalid-name
+        '''
+        Defines all custom actions for any keyboard presses. Overrides
+        keyPressEvents from MainWindow. This function must be called
+        exactly "keyPressEvent" in order to be processed properly by
+        PyQt.
+        '''
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+
+if __name__ == "__main__":
+    APP = QApplication(sys.argv)
+    ROOT = MainWindow()
+    ROOT.show()
+    sys.exit(APP.exec_())
